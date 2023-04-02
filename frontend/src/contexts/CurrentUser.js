4afa14c3a6e15697ec514ce377c267477fd3bf5e
Andres24680 <tasboyandresrestrepo@aol.com>
2023-04-02 12:05:07 -0400

    saved changes

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -6,6 +6,21 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
+<<<<<<< Updated upstream
+=======
+    useEffect(() => {
+
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:5001/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+  
+
+
+>>>>>>> Stashed changes
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
